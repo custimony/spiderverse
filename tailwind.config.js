042cadebd77bac6b6dfea0fcaaf231d4a0efee84
Habidoye97 <abidoyeabiodun97@gmail.com>
2023-06-01 10:53:26 +0100

    create html file for template one and install tailwind clip art

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    clipPath: {
+      triangle: 'polygon(50% 0%, 34% 30%, 66% 31%);',
+      card: 'circle(64.3% at 50% 50%);'
+
+    },
     extend: {
       colors: {
         'testimony-background-primary': 'rgb(241,240,238)',
@@ -14,6 +19,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-clip-path'),
+  ],
 }
 
