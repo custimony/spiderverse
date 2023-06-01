6cde8f8a5f8088ba8b9b94cbac79f358cbf61f05
Habidoye97 <abidoyeabiodun97@gmail.com>
2023-06-01 16:23:23 +0100

    Create the second template

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     clipPath: {
       triangle: 'polygon(50% 0%, 34% 30%, 66% 31%);',
-      card: 'circle(64.3% at 50% 50%);'
+      card: 'circle(64.3% at 50% 50%);',
+      arrow: 'polygon(0 0, 100% 0, 100% 50%, 100% 72%, 0 51%);',
+      arrow_invert: 'polygon(0 100%, 100% 100%, 100% 57%, 100% 72%, 0 18%);'
 
     },
     extend: {
@@ -13,6 +15,8 @@ module.exports = {
         'font-color-light': 'rgba(22,22,22,.65)',
         'font-color': '#161616',
         'border-color': '#f2f6ff',
+        'primary-color': '#ffc700',
+        'primary-white-opaq': 'rgba(215,101,158, 0.5)'
       },
       boxShadow: {
         'low': '0 10px 20px rgba(0, 0, 0, .04), 0 2px 6px rgba(0, 0, 0, .04)'
